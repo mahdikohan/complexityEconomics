21b5591127d604fb4b289dbcdce6e2b327df009a
mahdikohan <139677886+mahdikohan@users.noreply.github.com>
2023-07-17 21:07:52 +0330

    Update El_Farlo_Bar_sim.py

--- El_Farol_Bar/El_Farlo_Bar_sim.py
+++ El_Farol_Bar/El_Farlo_Bar_sim.py
@@ -2,3 +2,26 @@ number_of_people = 100
 attendency = 60
 number_of_strategy = 5
 
+class person:
+    def __init__(self) -> None:
+        self.strategy
+        self.forecastAtt
+
+class bar:
+    def __init__(self) -> None:
+        self.totalCap
+        
+class people:
+    def __init__(self) -> None:
+        self.members
+        self.attendance
+
+
+
+
+def selectRandom(people:list[list]):
+    return 0
+
+
+if __name__ == '__main__':
+    print('hi')
