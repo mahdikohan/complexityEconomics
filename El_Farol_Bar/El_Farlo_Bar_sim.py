06d956f25a8070a9d09702d05e0f18486b3dbae5
mahdikohan <139677886+mahdikohan@users.noreply.github.com>
2023-07-28 11:43:12 +0330

    Update El_Farlo_Bar_sim.py

--- El_Farol_Bar/El_Farlo_Bar_sim.py
+++ El_Farol_Bar/El_Farlo_Bar_sim.py
@@ -1,65 +1,77 @@
 import matplotlib.pyplot as plt
 import random
 import json
-
+import pandas as pd 
 
 
 ## Initial input variables for run simulaion
 
 # This var show total number of our society
-number_of_people = 50
+number_of_people = 1
 # This var shows pleasable attendance in bar
 attendance = 80
 # This var determine distinct number strategy that each person can have 
-number_of_strategy = 10
+number_of_strategy = 5
 # This var gives number of execusion of model in simulation
 ticks = 50
+# This var shows length of long memory
+length_of_long_memory = 3
 
-
-
-
-# if we consider decision is based on 
-# p(x) = w1 x(t1) + w2 x(t2) + ... + wn x(tn)
-def weight(h:int):
-    return ((2*random.random())-1)*h
     
+
+# longMemory
+class longMemory:
+    def __init__(self) -> None:
+        self.value:list
+    
+    def addExperience(self,experience:list) -> None:
+        self.value.append(experience)
+        self.value = self.value[-length_of_long_memory:]
+
 
 
 # Defined each identities
-class strategy:
-    def __init__(self, strategyId) -> None:
-        self.strategyId:int = strategyId
+def getStrategy():
+    strategy = set()
+    while True:
+        strategy.add((tuple([random.randrange(0,2,1) for i in range(0,length_of_long_memory)]),
+                      random.choice([-1,1])))
+        if len(strategy) == 2**length_of_long_memory:
+            break
+    
+    return list(strategy)
+
+
+
 
 
 
 
 
 class person:
-    def __init__(self, history, strategy, decision, attendance, shortMemory, longMemory) -> None:
-        self.history:list = history
-        self.strategy:strategy = strategy
+    def __init__(self, strategy, decision, attendance) -> None:
+        self.strategy:list = strategy
+        self.dfStrategies = []
         self.decision:bool = decision
         self.attendance:int = attendance
-        self.shortMemory:list = shortMemory
-        self.longMemory:list = longMemory
+        self.shortMemory:list = [0,0,0]
+        self.longMemory:list
 
     def __getStrategy(self)->int:
         last_strategy = list(map(weight,self.history[-self.strategy.strategyId:]))
         predicted = sum(last_strategy)
         return predicted < self.attendance
             
-    def putHistory(self, history:list):
-        self.history = history
-    
-    def appendHistory(self, attendanceCount:int):
-        self.history.append(attendanceCount)
-    
-    def updateHistory(self, index:int, value:int):
-        self.history[index] = value
-
     def takeDecision(self):
         forecastAttendance = self.__getStrategy()
         return forecastAttendance
+    
+    def refreshShortMemory(self) -> None:
+        self.shortMemory = [random.randrange(0,2,1) for i in range(1,4)]
+
+    def makeDFstrategy(self):
+        self.dfStrategies = [pd.DataFrame(self.strategy[i]) for i in range(0,number_of_strategy)]
+        print('df generated')
     
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
@@ -90,42 +102,46 @@ if __name__ == '__main__':
         #                                                     1)),
         #                 attendance = attendance,
         #                 decision=True)
-        borned = person(history=[],
-                        strategy= strategy(number_of_strategy),
+        borned = person(strategy =[getStrategy() for i in range(0,number_of_strategy)],
                         attendance = attendance,
                         decision=True)
         people.append(borned)
         i = i + 1
+
+        borned.makeDFstrategy()
+        for p in borned.dfStrategies:
+            print(p)
     
     # Start simulation
-    init_tick = ticks
+    # init_tick = ticks
 
-    while init_tick-ticks < init_tick:
-        history = []
+    # while init_tick-ticks < init_tick:
+    #     history = []
 
-        attendanceCounter = 0
-        tempAttendancePersonIndexes = []
+    #     attendanceCounter = 0
+    #     tempAttendancePersonIndexes = []
 
-        for j, p in enumerate(people):
-            if p.takeDecision():
-                attendanceCounter = attendanceCounter + 1
-                tempAttendancePersonIndexes.append(j)
-            # else:
-            #     p.appendHistory(60)
+    #     for j, p in enumerate(people):
+    #         if p.takeDecision():
+    #             attendanceCounter = attendanceCounter + 1
+    #             tempAttendancePersonIndexes.append(j)
+    #         # else:
+    #         #     p.appendHistory(60)
         
-        for k in tempAttendancePersonIndexes:
-            people[k].appendHistory(attendanceCounter)
+    #     for k in tempAttendancePersonIndexes:
+    #         people[k].appendHistory(attendanceCounter)
         
-        result  = len(tempAttendancePersonIndexes)
-        result_of_sim.append(result)
-        # print(result)
-        ticks = ticks - 1
+    #     result  = len(tempAttendancePersonIndexes)
+    #     result_of_sim.append(result)
+    #     # print(result)
+    #     ticks = ticks - 1
 
-        if ticks==1:
-            for p in people:
-                print(p.history)
+    #     if ticks==1:
+    #         for p in people:
+    #             print(p.history)
     
-    plt.plot(result_of_sim)
-    plt.show()
+    # plt.plot(result_of_sim)
+    # plt.show()
+
     # for p in people:
     #     print(p.toJSON())
