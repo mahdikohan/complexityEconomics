14ac363c40948cee2f11538f414e580a2daf4b29
mahdikohan <139677886+mahdikohan@users.noreply.github.com>
2023-07-29 20:35:15 +0330

    Update El_Farlo_Bar_sim.py

--- El_Farol_Bar/El_Farlo_Bar_sim.py
+++ El_Farol_Bar/El_Farlo_Bar_sim.py
@@ -8,37 +8,24 @@ import pandas as pd
 ### Initial input variables for run simulaion
 
 # This var show total number of our society
-number_of_people = 5
+number_of_people = 100
 # This var shows pleasable attendance in bar
-attendance = 80
+attendance = 20
 # This var determine distinct number strategy that each person can have 
 number_of_strategy = 5
 # This var gives number of execusion of model in simulation
-ticks = 2
+ticks = 100
 # This var shows length of long memory
 length_of_long_memory = 3
 
     
 
-# longMemory
-class longMemory:
-    def __init__(self) -> None:
-        self.value:list
-    
-    def addExperience(self,experience:list) -> None:
-        self.value.append(experience)
-        self.value = self.value[-length_of_long_memory:]
-
-
-
-
 class person:
-    def __init__(self, decision, attendance) -> None:
+    def __init__(self) -> None:
         self.strategies:list
-        self.decision:bool = decision
-        self.attendance:int = attendance
+        self.decision:dict = None
+        self.action:int
         self.shortMemory:list = [random.randrange(0,2,1) for i in range(0,length_of_long_memory)]
-        self.longMemory:list
 
 
     def __getStrategy(self)->list:
@@ -73,30 +60,42 @@ class person:
         lookup_result = self.__lookupStrategy(self.shortMemory)
         df_lookup_result = pd.DataFrame(lookup_result,columns=['strg','rfound','score'])
         max_score = df_lookup_result.loc[df_lookup_result['score'].idxmax()]
+        
+        # strategyId -> [0] list, [1] score -> rows
+        action = self.strategies[max_score['strg']][0][max_score['rfound']][1]
         result = {
                         'strategyId':max_score['strg'],
                         'score': max_score['score'],
-                        'rowIdFound': max_score['rfound']
+                        'rowIdFound': max_score['rfound'],
+                        'action':action
                    }
-        
+        self.action = result['action']
         self.decision = result
 
-        print(pd.DataFrame(lookup_result,columns=['strg','rfound','score']))
-        print(self.decision)
-        print("")
+        # print(pd.DataFrame(lookup_result,columns=['strg','rfound','score']))
+        # print(self.decision)
+        # print("")
 
         return self.decision
     
 
 
-    def getScore(self):
-        pass
+    def setScore(self, row_id, desired:int):
+        for i in range(0,len(self.strategies)):
+            if self.strategies[i][0][row_id][1] == desired:
+                self.strategies[i][1] = self.strategies[i][1] + 1
+
+
+
+    def updateShortMemory(self,g_t):
+        self.shortMemory.append(g_t)
+        self.shortMemory = self.shortMemory[-length_of_long_memory:]
     
 
 
     def makeDFstrategy(self):
         self.dfStrategies = [pd.DataFrame(self.strategies[i][0]) for i in range(0,number_of_strategy)]
-        print('df generated')
+        # print('df generated')
     
 
 
@@ -121,19 +120,8 @@ if __name__ == '__main__':
     result_of_sim = []
     i = 0
     while i < number_of_people:
-        # borned = person(history=[],
-        #                 strategy= strategy(random.randrange(1,
-        #                                                     number_of_strategy + 1,
-        #                                                     1)),
-        #                 attendance = attendance,
-        #                 decision=True)
-        borned = person(attendance = attendance,
-                        decision=True)
+        borned = person()
         people.append(borned)
-
-        # borned.makeDFstrategy()
-        # for p in borned.dfStrategies:
-        #     print(p)
         i = i + 1
 
 
@@ -141,10 +129,44 @@ if __name__ == '__main__':
     # Start simulation
     init_tick = ticks
 
+    
+
     while init_tick-ticks < init_tick:
+        gone = 0
+        A_t = 0
+        decision = None
+        desired = 0
+
         for agent in people:
-            agent.takeDecision()
+            decision = agent.takeDecision()
+
+            A_t = A_t + decision['action']
+
+            if decision['action'] == 1:
+                gone = gone + 1
+
+        
+        for agent in people:
+            if gone <= attendance:
+                agent.setScore(agent.decision['rowIdFound'], 1)
+            elif gone > attendance:
+                agent.setScore(agent.decision['rowIdFound'], -1)
             
+            g_t = -agent.action * A_t
+
+            if g_t < 0:
+                g_t = 0
+            elif g_t > 0:
+                g_t = 1
+
+            agent.updateShortMemory(g_t)
+            
+            
+
+        print(f"gone number :{gone}")
+
+        # stay [-1], go [1] are desired point
+        
         ticks = ticks - 1
 
     # while init_tick-ticks < init_tick:
