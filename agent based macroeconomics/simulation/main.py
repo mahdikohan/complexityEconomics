8233f7a8a98694ab9e85887bbab7abe315164aa1
mahdikohan <139677886+mahdikohan@users.noreply.github.com>
2023-08-17 20:41:07 +0330

    Update main.py

--- agent based macroeconomics/simulation/main.py
+++ agent based macroeconomics/simulation/main.py
@@ -1,12 +1,25 @@
-import pandas
+#   Considerations:
+##      Without a government or central banks
+##      Exclude growth
+##      
 
+
+# Initial variables
+num_households = 1000
+num_firms = 50
+
+# Define entities
 class household:
     def __init__(self) -> None:
         pass
 
+
+
 class firm:
     def __init__(self) -> None:
         pass
+
+
 
 households = []
 firms = []
