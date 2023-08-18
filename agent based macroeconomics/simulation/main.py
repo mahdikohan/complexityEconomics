9926aedf6c63f187a7d6cfb9d0550332f76a6a2c
mahdikohan <139677886+mahdikohan@users.noreply.github.com>
2023-08-18 19:31:13 +0330

    Update main.py

--- agent based macroeconomics/simulation/main.py
+++ agent based macroeconomics/simulation/main.py
@@ -13,7 +13,7 @@ days_of_month = 21              #days
 # Define entities
 class household:
     def __init__(self) -> None:
-        self.employee = None
+        self.employer = None
 
         # The reservation wage defines a minimal claim on labor income
         self.wage = None
@@ -28,16 +28,43 @@ class household:
         m_t_h = m_t_1 + income_t_1 - spending_t_1
         return m_t_h
     
+    # If the household is unemployed, he visits a randomly 
+    # chosen firm to check whether there is an open position.
+    def get_new_position(self):
+        pass
+    
 
+    # Households are picked in a random order to execute their goods demand.
+    def execute_demand(self):
+        pass
 
 
 class firm:
     def __init__(self) -> None:
         self.liquidity = None
         self.wage = None
+        self.inventory = None
+        self.employee = None
     
+
     # It means good prices
     def price(self):
+        pass
+
+
+
+    # firm has to decide on how to set its wage rate based on past
+    # success or failure to find workers at the offered wage rate.
+    def determine_wage_rate(self):
+        # Wage adjustment
+        pass
+
+    
+    def determine_number_employee(self):
+        pass
+
+
+    def determine_price(self):
         pass
 
     
@@ -46,3 +73,11 @@ class firm:
 
 households = []
 firms = []
+
+
+
+if __name__ == "__main__":
+    # begining of the month first step of simulation
+
+    # next step is starting the day
+    pass
