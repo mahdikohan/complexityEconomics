550bd223379bd979277203fdc9948d131655f6ce
mahdikohan <139677886+mahdikohan@users.noreply.github.com>
2023-08-21 14:41:32 +0330

    countinue

--- agent based macroeconomics/simulation/main.py
+++ agent based macroeconomics/simulation/main.py
@@ -51,12 +51,14 @@ class firm:
     def __init__(self) -> None:
         self.liquidity = None
         self.wage = random.randrange(15080,89000,70)
+        
         # certain critical bounds
         self.critical_inventory = [15080,(89000+15080)/2]
+
         self.inventory = random.randrange(15080,89000,70)
         self.employees = random.randrange(5,10)                 # Labor
     
-        # this just for push
+        
     # It means good prices
     def price(self):
         pass
