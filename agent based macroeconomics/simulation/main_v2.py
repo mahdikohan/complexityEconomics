f8459777d641b9a213a6a304e1605256b546f988
mahdikohan <139677886+mahdikohan@users.noreply.github.com>
2023-09-19 15:32:14 +0330

    Update main_v2.py
    add search jobs  by households

--- agent based macroeconomics/simulation/main_v2.py
+++ agent based macroeconomics/simulation/main_v2.py
@@ -10,6 +10,15 @@ num_firms = 10
 num_households = 100
 num_steps = 10  # Number of simulation steps
 
+
+
+
+
+
+
+
+
+
 # Initialize households with attributes and add them to the graph
 for household_id in range(num_households):
     wage = random.uniform(20000, 60000)
@@ -38,7 +47,7 @@ for firm_id in range(num_firms):
     G.add_node(f"Firm_{firm_id}", **firm_attrs)
 
 
-# recruitment ==============================
+# ============================ recruitment ==============================
 j = 0
 for firm_id in range(num_firms):
     # Randomly recruit a number of households
@@ -46,22 +55,46 @@ for firm_id in range(num_firms):
     
     if j<num_households:
         for recruit in range(j,j+i):
-            print(firm_id,recruit)
+            # print(firm_id,recruit)
             G.add_edge(f"Firm_{firm_id}", f"Household_{recruit}")
     j = j+i
 
-# checking number of firms for each households
-for household_id in range(num_households):
-    print(G.degree(f"Household_{household_id}"))
+# # checking number of firms for each households
+# for firm_id in range(num_firms):
+#     print(G.degree(f"Firm_{firm_id}"))
 
 
-
-
+pos = nx.spring_layout(G)
+node_colors = ['skyblue' if G.nodes[node]['type'] == "household" else 'red' for node in G.nodes()]
+nx.draw(G, pos, with_labels=True, node_color=node_colors, font_color='black', node_size=500)
+plt.show()
 
 
 # Simulation loop
 for step in range(num_steps):
-    print(f"Step {step + 1}:")
+    print(f"Step {step + 1}: Done")
+
+    # each households check some firms wage
+    for household_id in range(num_households):
+
+        samplef = random.randint(1, num_firms)
+        for firm_id in range(samplef,num_firms):
+            if (G.nodes[f"Firm_{firm_id}"]['wage']) > (G.nodes[f"Household_{household_id}"]['wage']):
+                # print(f'new connection household_{household_id}, firm_{firm_id}')
+                G.remove_edges_from(list(G.edges(f"Household_{household_id}")))
+                G.add_edge(f"Household_{household_id}",f"Firm_{firm_id}")
+                G.nodes[f"Firm_{firm_id}"]['wage'] = G.nodes[f"Household_{household_id}"]['wage']
+                continue
+
+            # print('*** Degree: '+str(G.degree(f"Household_{household_id}")))
+            
+            if G.degree(f"Household_{household_id}") == 0:
+                print(f'unemployment new connection household_{household_id}, firm_{firm_id}')
+                G.add_edge(f"Household_{household_id}",f"Firm_{firm_id}")
+                G.nodes[f"Firm_{firm_id}"]['wage'] = G.nodes[f"Household_{household_id}"]['wage']
+
+
+
 
 
 
